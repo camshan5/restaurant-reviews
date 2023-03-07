71b73b60baf9d5a5132a2c7a6ae7ea6af449365f
Cameron Shannon <cshannon@choicepays.com>
2023-03-07 09:43:30 -0800

    Set up mongo client & express server

---
+++ backend/index.js
@@ -0,0 +1,29 @@
+import mongodb from "mongodb";
+import dotenv from "dotenv";
+import app from "./server.js";
+
+// load environment variables
+dotenv.config();
+
+// get access to mongo client
+const MongoClient = mongodb.MongoClient;
+
+// set port from environment variables or default to 8000
+const port = process.env.PORT || 8000;
+
+// connect to db and start the server
+MongoClient.connect(process.env.RESTREVIEWS_DB_URI, {
+  poolSize: 50, // max 50 people can connect at a time
+  wtimeout: 2500, // wait 2500ms before giving up on connection
+  useNewUrlParser: true,
+})
+  .catch((err) => {
+    console.log(err.stack);
+    process.exit(1);
+  })
+  .then(async (client) => {
+    // starting webserver after db is connected
+    app.listen(port, () => {
+      console.log(`listening on port ${port}`);
+    });
+  });
--- backend/server.js
+++ backend/server.js
@@ -7,3 +7,11 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+// configure routes
+app.use('/api/v1/restaurants', restaurants);
+
+// incorrect route error handling
+app.use('*', (req, res) => res.status(404).json({ error: 'not found' }));
+
+export default app
